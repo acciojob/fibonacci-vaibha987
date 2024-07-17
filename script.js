b38785fe0a433b6bdbc1c52e18c0b6ee1ca6d478
vaibha987 <v2goyal95@gmail.com>
2024-07-17 12:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function fibonacci(num) {
-  let x = 0;
-  let y = 1;
-  let z
-  for(let i = 2; i <= num; i++) {
-    let z = x + y;
-    x = y;
-    y = z;
-  }
-
-  return z
+  let num1 = 0;
+    let num2 = 1;
+    let sum;
+    if (num === 1) {
+        return num1;
+    } else if (num === 2) {
+        return num2;
+    } else {
+        for (let i = 3; i <= num; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2;
+    }
 }
 
 module.exports = fibonacci;
