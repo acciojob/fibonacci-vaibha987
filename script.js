83fcd8ac2de2b266575f5028a575670af3cc54c6
vaibha987 <v2goyal95@gmail.com>
2024-08-14 21:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function fibonacci(N) // returns the Nth Fibonacci Number
+function fibonacci(num) // returns the Nth Fibonacci Number
 {
 let a = 0; // Pos 1 || 1st Finonacci Number || Position 1 in the Fibo. Series 
 let b = 1;  // Pos 2 || 2nd Finonacci Number || Position 2 in the Fibo. Series 
 let c = 0; // Should be position 3
 let currentPosition = 3;
-while (currentPosition <= N) { // Exit when N+1 happens; so c would've been calculated as Position Nth in the Fibo. Series 
+while (currentPosition <= num) { // Exit when N+1 happens; so c would've been calculated as Position Nth in the Fibo. Series 
     c = a + b;
      a = b; // n-2th
       b = c; // n-1th value
